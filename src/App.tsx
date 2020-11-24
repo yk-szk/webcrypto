77395cbe79d0f23f36d258148bc2eef1d408491b
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-11-24 19:02:31 +0900

    Create first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { KeyManager } from './KeyManager';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header"></header>
+        <body>
+          <Container maxWidth="lg">
+            <Typography component="h1" variant="h2">
+              Webcrypto
+            </Typography>
+            <KeyManager />
+          </Container>
+        </body>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/KeyManager.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export function KeyManager() {
+  return (
+    <React.Fragment>
+      <Typography component="h2" variant="h3">
+        KeyManager
+      </Typography>
+    </React.Fragment>
+  );
+}
